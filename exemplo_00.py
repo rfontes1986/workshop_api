2ca84c68e3e8c692e7471240e9a7fb09a80ecc60
Rafael Cavalcanti <rfontes86@gmail.com>
2026-06-04 13:29:39 +0100

    updated workshop api

---
+++ exemplo_00.py
@@ -0,0 +1,15 @@
+import random
+import time
+
+def numero_aleatorio():
+    return random.randint(1, 10)
+
+def dobra_um_numero(num):
+    return num * 2
+
+if __name__ == "__main__":
+    while True:
+        num = numero_aleatorio()
+        resultado = dobra_um_numero(num)
+        print (f"O dobro de {num} é {resultado}")
+        time.sleep(1)
